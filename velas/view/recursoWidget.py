08a53f42188b1c18430fc8a94b7bd38b1ea55b91
IngErnestoAlvarez <ernesto.alvarez1812@gmail.com>
2020-10-18 14:46:23 -0300

    Refactorizaciones recursoWidget

--- velas/view/recursoWidget.py
+++ velas/view/recursoWidget.py
@@ -8,36 +8,73 @@ class WidgetRecurso(QtWidgets.QWidget):
 
     def __init__(self, recurso:Recurso) -> None:
         QtWidgets.QWidget.__init__(self)
-        self.setMaximumHeight(50)
         self.rec = recurso
+        self.setUp()
+
+        # NOMBRE
+        self.nombre = self.iniciarNombre(self.rec.nombre)
+        self.layout.addWidget(self.nombre)
+
+        # PRECIO
+        self.precio = self.iniciarPrecio(self.rec.precio)
+        self.layout.addWidget(self.precio)
+
+        # BOTON DE ELIMINADO
+        self.buttonDelete = self.iniciarButtonDelete()
+        self.buttonDelete.clicked.connect(self.messageButtonDelete)
+        self.layout.addWidget(self.buttonDelete)
+
+        # BOTON DE AGREGAR STOCK
+        self.buttonAdd = self.iniciarButtonAdd()
+        self.buttonAdd.clicked.connect(self.faddStock)
+        self.layout.addWidget(self.buttonAdd)
+    
+    def setUp(self):
+        self.setMaximumHeight(50)
         self.layout = QtWidgets.QHBoxLayout()
         self.setLayout(self.layout)
         self.layout.setSpacing(10)
 
-        self.nombre = QtWidgets.QLabel()
-        self.nombre.setText(self.rec.nombre)
-        self.nombre.setAccessibleName("widgetRecursoInicio")
-        self.nombre.setMaximumWidth(100)
-        self.layout.addWidget(self.nombre)
-
-        # BOTON DE ELIMINADO
-        self.buttonDelete = QtWidgets.QPushButton()
-        buttonIcon = QIcon(r"resources\imagenes\basura.png")
-        self.buttonDelete.setIcon(buttonIcon)
-        self.buttonDelete.setMaximumSize(50,50)
-        self.buttonDelete.setAccessibleName("botonBasura")
-        self.buttonDelete.clicked.connect(self.messageButton)
-        self.layout.addWidget(self.buttonDelete)
-
-        # BOTON DE AGREGAR STOCK
-        self.buttonAdd = QtWidgets.QPushButton()
-        buttonAddIcon = QIcon(r"resources\imagenes\mas.png")
-        self.buttonAdd.setIcon(buttonAddIcon)
-        self.buttonAdd.setMaximumSize(50,50)
-        self.buttonAdd.setAccessibleName("botonBasura")
-        self.buttonAdd.clicked.connect(self.faddStock)
-        self.layout.addWidget(self.buttonAdd)
+    @staticmethod
+    def iniciarNombre(nombre:str) -> QtWidgets.QLabel:
+        nombreAux = QtWidgets.QLabel()
+        nombreAux.setText(nombre)
+        nombreAux.setAccessibleName("widgetRecursoInicio")
+        nombreAux.setMaximumWidth(100)
+        nombreAux.setToolTip("Nombre del recurso")
+        return nombreAux
     
+    @staticmethod
+    def iniciarButtonDelete() -> QtWidgets.QPushButton:
+        buttonDelete = QtWidgets.QPushButton()
+        buttonIcon = QIcon(r"resources\imagenes\basura.png")
+        buttonDelete.setIcon(buttonIcon)
+        buttonDelete.setMaximumSize(50,50)
+        buttonDelete.setAccessibleName("botonBasura")
+        buttonDelete.setToolTip("Eliminar recurso")
+        return buttonDelete
+
+    @staticmethod
+    def iniciarButtonAdd() -> QtWidgets.QPushButton:
+        buttonAddAux = QtWidgets.QPushButton()
+        buttonAddIcon = QIcon(r"resources\imagenes\mas.png")
+        buttonAddAux.setIcon(buttonAddIcon)
+        buttonAddAux.setMaximumSize(50,50)
+        buttonAddAux.setAccessibleName("botonBasura")
+        buttonAddAux.setToolTip("Añadir stock.")
+        return buttonAddAux
+    
+    @staticmethod
+    def iniciarPrecio(precio:float) -> QtWidgets.QLabel:
+        precioAux = QtWidgets.QLabel()
+        precioAux.setText("$"+str(precio))
+        precioAux.setAccessibleName("widgetRecursoInicio")
+        precioAux.setMaximumWidth(60)
+        precioAux.setToolTip("Precio del recurso")
+        precioAux.setStyleSheet("QLabel{font: 16px;}")
+        return precioAux
+
+
     def cargarEnLayout(self, destino:QBoxLayout) -> None:
         '''Se le pasa como parametro una QBoxLayout y te carga dentro de ella un WidgetRecurso'''
         destino.addWidget(self)
@@ -48,13 +85,14 @@ class WidgetRecurso(QtWidgets.QWidget):
         p = QPainter(self)
         self.style().drawPrimitive(QStyle.PE_Widget, opt, p, self)
     
-    def messageButton(self):
+    def messageButtonDelete(self):
         msg = QMessageBox()
         msg.setIcon(QMessageBox.Warning)
         msg.setText("Esta a punto de eliminar un recurso")
         msg.setInformativeText("Se elminará todo registro de él, incluyendo cantidades y precios.")
         msg.setWindowTitle("Eliminar recurso")
-        msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+        msg.addButton(QMessageBox.Ok)
+        msg.addButton(QMessageBox.Cancel)
         msg.buttonClicked.connect(self.eliminar)
 
         msg.exec_()
